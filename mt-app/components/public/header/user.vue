413b1911c18bfaa4e12982e9999cb1433735423c
xuxinfu1994 <870852780@qq.com>
2019-04-29 17:36:50 +0800

    '项目'

---
+++ mt-app/components/public/header/user.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="m-user">
+    <template v-if="user">
+      欢迎您，<span class="username">{{ user }}</span> [<nuxt-link to="/exit"
+        >退出</nuxt-link
+      >]
+    </template>
+    <template v-else>
+      <nuxt-link to="/login" class="login">立即登录</nuxt-link>
+      <nuxt-link class="register" to="/register">注册</nuxt-link>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: ''
+    }
+  },
+  async mounted() {
+    const {status,data:{user}} = await  this.$axios.get('http://127.0.0.1:3000/users/getUser')
+    if(status===200){
+      this.user=window.decodeURIComponent(user)
+    }
+  },
+}
+</script>
+
+<style lang="css"></style>
---
+++ mt-app/server/interface/utils/sign.js
@@ -0,0 +1,2 @@
+const sign = 'd587b84531845a2a8f598ac9ff14668e'//请填写自己的签名
+export default sign
